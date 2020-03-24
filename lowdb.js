f54f3b55a0b6b5652d2b188fbf9071b53b186950
Nguyen Sy Canh Hung <vltnschu@MOVmac130.local>
2020-03-24 21:57:05 +0700

    demo using Ajax

--- lowdb.js
+++ lowdb.js
@@ -1,16 +1,57 @@
-import low from './node_modules/lowdb'
-import FileSync from './node_modules/lowdb/adapters/FileSync.js'
+$(document).ready(function(){
+    $('.btn-show').click(function(){
+        $.ajax({
+            url : 'http://localhost:3000/income_list',
+            method : 'GET',
+            dataType : 'JSON',
+            success : function(data){
+                console.log(data[0].value);
+                
+            }
+        })
+})
+    $('.btn-add').click(function(){
+        var local_inc = JSON.parse(localStorage.data).allItems['inc'];
+        for(let i = 0 ; i < local_inc.length; i++)
+        {
+            $.ajax({
+                url : `http://localhost:3000/income_list`,
+                method : 'POST',
+                ContentType : 'application/json',
+                data : JSON.parse(localStorage.data).allItems['inc'][i] ,
+                success : function(){
+                    alert('add data completed');
+                }
+            })
+        }
+        
+    })
 
- const adapter = new FileSync('db.json')
- const db = low(adapter)
+    $('.btn-del').click(function(){
+        var local_inc = JSON.parse(localStorage.data).allItems['inc'];
+        for(let i = 0 ; i < local_inc.length; i++)
+        {
+            $.ajax({
+                url : `http://localhost:3000/income_list/${i+1}`,
+                method : 'DELETE',
+                success : function(){
+                    alert('delete data completed');
+                }
+            })
+        }
+        
+    })
 
-
- db.defaults({ 
-     "author" : {
-         "name" : "Nguyen Sy Canh Hung",
-         "age" : 21,
-     },
-     "income_list" : [],
-     "expense_list" : []
-  })
- .write()
+    $('.btn-update').click(function(){
+        $.ajax({
+            url : 'http://localhost:3000/income_list/1',
+            method : 'PUT',
+            contentType : 'application/json',
+            dataType : 'JSON',
+            data : JSON.parse(localStorage.data).allItems['inc'][0],
+            success : function(){
+                alert('update data success');
+            }
+        })
+})
+})  
